2d43d74f4dbf7ccb0cec183d41eb3ae0ac546bb3
tmosco <tmosco_tayo@yahoo.com>
2022-01-07 09:56:01 +0100

    use use react hook form to validate the form

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext ,useEffect} from 'react';
+import React, { useContext, useEffect } from 'react';
 import Layout from '../components/Layout';
 import NextLink from 'next/link';
 import {
@@ -14,10 +14,16 @@ import axios from 'axios';
 import { Store } from '../utils/Store';
 import Router, { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
+import { Controller, useForm } from 'react-hook-form';
 
 export default function Login() {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
   const router = useRouter();
-  const {redirect} = router.query;
+  const { redirect } = router.query;
 
   const { state, dispatch } = useContext(Store);
   const { userInfo } = state;
@@ -27,11 +33,8 @@ export default function Login() {
     }
   }, []);
 
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
   const classes = useStyles();
-  async function submitHandler(e) {
-    e.preventDefault();
+  async function submitHandler({ email, password }) {
     try {
       const { data } = await axios.post('/api/users/login', {
         email,
@@ -46,30 +49,68 @@ export default function Login() {
   }
   return (
     <Layout title="Login">
-      <form onSubmit={submitHandler} className={classes.form}>
+      <form onSubmit={handleSubmit(submitHandler)} className={classes.form}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
         <List>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="email"
-              label="Email"
-              inputProps={{ type: 'email' }}
-              onChange={(e) => setEmail(e.target.value)}
-            ></TextField>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  inputProps={{ type: 'email' }}
+                  {...field}
+                  error={Boolean(errors.email)}
+                  helperText={
+                    errors.email
+                      ? errors.email.type === 'pattern'
+                        ? 'Email is not valid'
+                        : 'Email is required'
+                      : ''
+                  }
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="password"
-              label="Password"
-              inputProps={{ type: 'password' }}
-              onChange={(e) => setPassword(e.target.value)}
-            ></TextField>
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength:6
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  inputProps={{ type: 'password' }}
+                  {...field}
+                  error={Boolean(errors.password)}
+                  helperText={
+                    errors.password
+                      ? errors.password.type === 'minLength'
+                        ? 'Password length is more than 5'
+                        : 'Password is required'
+                      : ''
+                  }
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
             <Button variant="contained" type="submit" fullWidth color="primary">
