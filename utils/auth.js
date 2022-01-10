21ec74f5382a79f23fb5ef0259fca4c98f3ce130
tmosco <tmosco_tayo@yahoo.com>
2022-01-10 12:50:03 +0100

    create onError and isAuth to verify user and create orders

--- utils/auth.js
+++ utils/auth.js
@@ -15,5 +15,22 @@ const signToken = (user) => {
   );
 };
 
+const isAuth =async(req,res,next) =>{
+const {authorization} = req.headers;
+if(authorization){
+    const token = authorization.slice(7, authorization.lenth);
+    jwt.verify(token, process.env.JWT_SECRET, (err,decode)=>{
+        if(err){
+            res.status(401).send({message:"Token is not valid"});
+        }else{
+            req.user = decode;
+            next();
+        }
+    })
+}else{
+    res.status(401).send({message:"Token is not supplied"});
+}
+}
 
-export {signToken};
+
+export {signToken, isAuth};
