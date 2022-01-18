eac93f257b9d21fad2fe577d7b05941ca8823356
tmosco <tmosco_tayo@yahoo.com>
2022-01-18 19:37:40 +0100

    create the delivery api and button

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -18,6 +18,7 @@ import {
   Card,
   List,
   ListItem,
+  Button,
 } from '@material-ui/core';
 import axios from 'axios';
 import { useRouter } from 'next/router';
@@ -69,7 +70,7 @@ function Order({ params }) {
   const { userInfo } = state;
 
   const [
-    { loading, error, order, successPay },
+    { loading, error, order, successPay, loadingDeliver, successDeliver },
     dispatch,
   ] = useReducer(reducer, {
     loading: true,
@@ -104,10 +105,18 @@ function Order({ params }) {
         dispatch({ type: 'FETCH_FAIL', payload: getError(err) });
       }
     };
-    if (!order._id || successPay || (order._id && order._id !== orderId)) {
+    if (
+      !order._id ||
+      successPay ||
+      successDeliver ||
+      (order._id && order._id !== orderId)
+    ) {
       fetchOrder();
-      if(successPay){
-          dispatch({type:'PAY_RESET'});
+      if (successPay) {
+        dispatch({ type: 'PAY_RESET' });
+      }
+      if (successDeliver) {
+        dispatch({ type: 'DELIVER_RESET' });
       }
     } else {
       const loadPaypalScript = async () => {
@@ -125,7 +134,7 @@ function Order({ params }) {
       };
       loadPaypalScript();
     }
-  }, [order, successPay]);
+  }, [order, successPay, successDeliver]);
   const { enqueueSnackbar } = useSnackbar();
 
   function createOrder(data, actions) {
@@ -162,20 +171,29 @@ function Order({ params }) {
     });
   }
 
-
-
-
-
-
-
-  
-
   function onError(err) {
     enqueueSnackbar(getError(err), { variant: 'error' });
   }
 
+  async function deliverOrderHandler() {
+    try {
+      dispatch({ type: 'DELIVER_REQUEST' });
+      const { data } = await axios.put(
+        `/api/orders/${order._id}/delivered`,
+        {},
+        {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({ type: 'DELIVER_SUCCESS', payload: data });
+      enqueueSnackbar('Order is delivered', { variant: 'success' });
+    } catch (err) {
+      dispatch({ type: 'DELIVER_FAIL', payload: getError(err) });
+    }
+  }
+
   return (
-    <Layout title={`Order ${orderId}`}> 
+    <Layout title={`Order ${orderId}`}>
       <Typography component="h1" variant="h1">
         Order {orderId}
       </Typography>
@@ -321,15 +339,27 @@ function Order({ params }) {
                     {isPending ? (
                       <CircularProgress />
                     ) : (
-                        <div className="{classes.fullWidth}">
-
-                      <PayPalButtons
-                        createOrder={createOrder}
-                        onApprove={onApprove}
-                        onError={onError}
+                      <div className="{classes.fullWidth}">
+                        <PayPalButtons
+                          createOrder={createOrder}
+                          onApprove={onApprove}
+                          onError={onError}
                         ></PayPalButtons>
-                        </div>
+                      </div>
                     )}
+                  </ListItem>
+                )}
+                {userInfo.isAdmin && order.isPaid && !order.isDelivered && (
+                  <ListItem>
+                    {loadingDeliver && <CircularProgress />}
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      color="primary"
+                      onClick={deliverOrderHandler}
+                    >
+                      Deliver Order
+                    </Button>
                   </ListItem>
                 )}
               </List>
